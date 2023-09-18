8084a9922a540b39324da2f6a03350bcac979301
Alice2506 <ralko84@mail.ru>
2023-09-18 19:12:17 +0300

    added task 2

--- JSbase10/index.js
+++ JSbase10/index.js
@@ -14,18 +14,52 @@ keyin7: 7,
 */
 
 const numbers = {
-    keyin1: 1,
-    keyin2: 2,
-    keyin3: 3,
-    keyin4: 4,
-    keyin5: 5,
-    keyin6: 6,
-    keyin7: 7,
-    };
+  keyin1: 1,
+  keyin2: 2,
+  keyin3: 3,
+  keyin4: 4,
+  keyin5: 5,
+  keyin6: 6,
+  keyin7: 7,
+};
 
 for (const key in numbers) {
-    if (numbers[key] >= 3) {
-        console.log(numbers[key]);
-    }
+  if (numbers[key] >= 3) {
+    console.log(numbers[key]);
+  }
 }
 
+/* 
+Задание 2
+Необходимо из объекта, который лежит в константе post вывести значения, к которым приписан комментарий, в консоль.
+*/
+
+const post = {
+  author: "John", // вывести этот текст
+  postId: 23,
+  comments: [
+    {
+      userId: 10,
+      userName: "Alex",
+      text: "lorem ipsum",
+      rating: {
+        likes: 10,
+        dislikes: 2, // вывести это число
+      },
+    },
+    {
+      userId: 5, // вывести это число
+      userName: "Jane",
+      text: "lorem ipsum 2", // вывести этот текст
+      rating: {
+        likes: 3,
+        dislikes: 1,
+      },
+    },
+  ],
+};
+
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
